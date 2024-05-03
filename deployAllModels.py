46885f40b1ff92204501ec43a2fa7a63d1621ff4
KHCCResearch <165085489+KHCCResearch@users.noreply.github.com>
2024-05-03 19:46:48 +0300

    Update deployAllModels.py

--- deployAllModels.py
+++ deployAllModels.py
@@ -19,12 +19,8 @@ from tensorflow.keras.layers import Bidirectional
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.models import load_model
-import subprocess
-import os
-#if not os.path.isfile('model.h5'):
-subprocess.run(['curl --output model.h5 "https://github.com/KHCCResearch/testSVM/BiLSTMSentimenttrained_model.h5"'], shell=True)
-# loading the saved model
-BiLSTM_model=load_model('model.h5', compile=False)
+
+BiLSTM_model=load_model('BiLSTMSentimenttrained_model.h5', compile=False)
 
 
 loaded_model = pickle.load(open('Sentimenttrained_model2.sav', 'rb'))
